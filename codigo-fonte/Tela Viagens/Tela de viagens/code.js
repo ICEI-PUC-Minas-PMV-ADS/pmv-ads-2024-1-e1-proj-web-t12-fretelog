1c989d700e957d6d949cc380daf165bb3a08309c
Kayky <kayky.freitas@sga.pucminas.br>
2024-06-08 12:49:47 -0300

    Tela de Viagens da Transportadora

---
+++ codigo-fonte/Tela Viagens/Tela de viagens/code.js
@@ -0,0 +1,17 @@
+const dadosUsuario = {
+    nome: "Fulano da Silva",
+    email: "fulano@email.com",
+    estado: "Minas Gerais",
+    contato: "(31)4002-8922"
+  };
+  localStorage.setItem('dadosUsuario', JSON.stringify(dadosUsuario));
+  const dadosArmazenados = localStorage.getItem('dadosUsuario');
+  const usuario = JSON.parse(dadosArmazenados);
+  const espaco1 = document.getElementById('quadrado1');
+  espaco1.innerHTML = `${usuario.nome}`;
+  const espaco2 = document.getElementById('quadrado2');
+  espaco2.innerHTML = `${usuario.email}`;
+  const espaco3 = document.getElementById('quadrado3');
+  espaco3.innerHTML = `${usuario.estado}`;
+  const espaco4 = document.getElementById('quadrado4');
+  espaco4.innerHTML = `${usuario.contato}`;
